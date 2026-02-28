3fd066e92cff91896e52499c06923b74533fa18e
Julien <julien.etelain@outlook.com>
2026-02-28 10:19:05 +0100

    Apply suggestions from code review

--- src/Pmad.Wiki/WikiServiceCollectionExtensions.cs
+++ src/Pmad.Wiki/WikiServiceCollectionExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Razor;
+﻿using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Pmad.Git.HttpServer;
