10c8215e375e8a4d9d4d96a38a08505f099c9bf4
Julien <julien.etelain@outlook.com>
2026-02-28 10:10:41 +0100

    Move admin actions to a dedicated controller

---
+++ src/Pmad.Wiki/WikiViewLocationExpander.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Pmad.Wiki;
+
+internal sealed class WikiViewLocationExpander : IViewLocationExpander
+{
+    public void PopulateValues(ViewLocationExpanderContext context) 
+    { 
+    }
+
+    public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+    {
+        if (context.ControllerName is "WikiAdmin")
+        {
+            return viewLocations.Select(l => l.Replace("{1}", "Wiki"));
+        }
+        return viewLocations;
+    }
+}
